b2bf210879b115be6412bf030318a8140a965bb6
Dev0Dyssey <tmuk@protonmail.com>
2019-04-07 19:43:24 +0100

    Added Bootstrap 4 styling and css button style

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Counter from './Counters'
+import './style.css'
 
 const App = ({count}) => {
     return (
-        <>
-            <h1>Counter</h1>
-            <div>Count: {count}</div>
-            <br/>
-            <Counter/>
-        </>
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>Counter</h1>
+                    <div>Count: {count}</div>
+                    <br/>
+                    <Counter/>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/Counters.js
+++ src/components/Counters.js
@@ -5,13 +5,33 @@ import { addButton, subtractButton, addDelayButton, subtractDelayButton } from '
 class Counter extends React.Component {
     render() {
         return(
-            <>
-            <button onClick={() => this.props.addButton()}>Add</button>
-            <button onClick={() => this.props.subtractButton()}>Subtract</button>
-            <br/>
-            <button onClick={() => this.props.addDelayButton()}>Add (1s delay)</button>
-            <button onClick={() => this.props.subtractDelayButton()}>Subtract (1s delay)</button>
-            </>
+            <div>
+                <button 
+                    type="button" 
+                    className="btn btn-primary" 
+                    onClick={() => this.props.addButton()}>
+                        Add
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-danger" 
+                    onClick={() => this.props.subtractButton()}>
+                        Subtract
+                </button>
+                    <br/>
+                <button
+                    type="button"
+                    className="btn btn-success" 
+                    onClick={() => this.props.addDelayButton()}>
+                        Add (1s delay)
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-warning"
+                    onClick={() => this.props.subtractDelayButton()}>
+                        Subtract (1s delay)
+                </button>
+            </div>
         );
     };
 }
